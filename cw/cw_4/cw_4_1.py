a42f036e64b6f4b7e30433e8d679dfd5255a3ea1
SKO7OPENDRA <58439768+SKO7OPENDRA@users.noreply.github.com>
2020-02-02 02:48:22 +0300

    release-v1.2

--- cw/cw_4/cw_4_1.py
+++ cw/cw_4/cw_4_1.py
@@ -4,5 +4,5 @@ x = 2 + 2
 print(timeit.timeit('x = 2 + 2'))
 print(timeit.timeit('x=sum(range(10))'))
 
-# cd cw/cw-4
-# python -m timeit -n 100 -s "import cw-4.1"
+# cd cw/cw
+# python -m timeit -n 100 -s "import cw.1"
